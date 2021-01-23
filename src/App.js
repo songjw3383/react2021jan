c0e50bbc2531727cd64ab6a41d44d6f4a54ba832
songjw3383 <56250064+songjw3383@users.noreply.github.com>
2021-01-24 00:32:46 +0900

    0124

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state ={
-      mode:'welcome',
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
@@ -19,7 +19,6 @@ class App extends Component {
     }
   }
   render() {
-    
     var _title, _desc = null;
     if(this.state.mode ==='welcome'){
       _title = this.state.welcome.title;
@@ -29,12 +28,24 @@ class App extends Component {
       _desc = this.state.contents[0].desc;
 
     }
+  
   return (
     <div className="App">
-      <Subject 
+      {/*<Subject 
       title={this.state.subject.title} 
       sub={this.state.subject.sub}>   
-      </Subject>
+      </Subject> */}
+      <header>
+            <h1><a href="/" onClick={function(e){
+                console.log(e);
+                e.preventDefault();
+                //this.state.mode = 'welcome';
+                this.setState({
+                  mode:'welcome'
+                })
+            }.bind(this)}>{this.state.subject.title}</a></h1>
+            {this.state.subject.sub}
+      </header>
       <TOC data={this.state.contents}></TOC>
       <Content title={_title} desc={_desc}></Content>
     </div>
