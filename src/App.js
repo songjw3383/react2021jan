704e22090719ba41d70cb2cc23290e6028ee33b5
songjw3383 <56250064+songjw3383@users.noreply.github.com>
2021-01-26 23:40:03 +0900

    0126

--- src/App.js
+++ src/App.js
@@ -1,56 +1,88 @@
 import React, { Component } from 'react';
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
+import CreateContent from "./components/CreateContent";
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={
-      mode:'read',
-      subject:{title:'WEB', sub:'World Wide Web!'},
-      welcome:{title:'Welcome', desc:'Hello, React!!'},
-      contents:[
-        {id:1, title:'HTML', desc: 'HTML is for information'},
-        {id:2, title:'CSS', desc: 'CSS is for design'},
-        {id:3, title:'JavaScript', desc: 'JavaScript is for interactive'},
+    this.max_content_id = 3;
+    this.state = {
+      mode: 'create',
+      selected_content_id:2,
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      welcome: { title: 'Welcome', desc: 'Hello, React!!' },
+      contents : [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' },
       ]
     }
   }
   render() {
-    var _title, _desc = null;
-    if(this.state.mode ==='welcome'){
+    var _title, _desc, _article= null;
+    if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    } else if (this.state.mode ==='read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
-
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if (this.state.mode === 'read') {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title,_desc){
+        this.max_content_id = this.max_content_id+1;
+        //this.state.contents.push(
+        //  {id:this.max_conetent_id, title:_title, desc:_desc}); -> 이 방식은 원래의 데이터를 아예 변경시킴, 하지만 concat은 본래의 데이터를 유지시킨다.
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          contents:_contents
+        });
+        // add content to this.state.contents, 컨텐츠에 내용 추가
+      }.bind(this)}></CreateContent>
     }
-  
-  return (
-    <div className="App">
-      {/*<Subject 
-      title={this.state.subject.title} 
-      sub={this.state.subject.sub}>   
-      </Subject> */}
-      <header>
-            <h1><a href="/" onClick={function(e){
-                console.log(e);
-                e.preventDefault();
-                //this.state.mode = 'welcome';
-                this.setState({
-                  mode:'welcome'
-                })
-            }.bind(this)}>{this.state.subject.title}</a></h1>
-            {this.state.subject.sub}
-      </header>
-      <TOC data={this.state.contents}></TOC>
-      <Content title={_title} desc={_desc}></Content>
-    </div>
-  );
-}
+
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}
+        >
+        </Subject>
+        <TOC 
+        onChangePage={function (id) {
+          this.setState({
+            mode: 'read',
+            selected_content_id: Number(id)
+          });
+        }.bind(this)}
+          data={this.state.contents}
+          ></TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          })
+        }.bind(this)}></Control>
+        {_article}
+      </div>
+    );
+  }
 }
 
 export default App;
